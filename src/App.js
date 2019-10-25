f074dc5fbb76c2f00e755a2ff2a3dfa9a70695f3
Chase Holzer <32146471+ckholzer27@users.noreply.github.com>
2019-10-25 17:04:36 -0500

    pre-existing code
    Everything from previous project with same design as Recipeazy

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import AccountHome from './components/accountPage/AccountHome';
+import Login from './components/Login';
+import WorkoutGenerator from './components/WorkoutGenerator';
+import WorkoutPage from './components/WorkoutPage';
+import StopWatch from './components/StopWatch';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AccountHome/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Bootstrap from "react-bootstrap";
+import './Login.css';
+import Navigation from './Navigation';
+import { WorkoutGeneratorRepository } from '../api/workoutGenRepo';
+import Logo from '../assets/logo.png';
+
+
+function FailedLogin(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <div class="alert alert-primary" role="alert">
+  Login FAAAILED
+</div>;
+  }
+  return <></>;
+}
+
+
+
+class Login extends Component {
+  workoutGeneratorRepo = new WorkoutGeneratorRepository;
+
+  constructor(props) {
+        super(props);
+
+        this.state = {
+          username: "",
+          password: "",
+          failed_login:false
+        };
+
+  };
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // "lifter97","password"
+    this.workoutGeneratorRepo.getUserId(this.state.username,this.state.password).then(
+
+      login_success => {
+
+        if (login_success){
+          //add redirect later
+          alert("Login success will redirect now..")
+        }
+        else{
+          this.setState({ failed_login: true })
+        }
+      }
+    );
+    // login_success = this.workoutGeneratorRepo.getUserId(this.state.email, this.state.password);
+    //add redirect if login success not null
+    // else show login failed
+
+  }
+  render() {
+    return (
+      <>
+      < Navigation hideNav={true} />
+
+
+      <img
+                  alt=""
+                  src={Logo}
+                  width="300"
+                  className="d-inline-block align-top"
+                  />
+                <p/>
+      <h2> Sign in to Anyfit</h2>
+      <div id="login" className="mx-auto">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Label>Username</Form.Label>
+              <Form.Group controlId="username" bsSize="large">
+                <Form.Control
+                  autoFocus
+                  type="username"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+                <Form.Text className="text-muted">
+      We'll never share your email with anyone except Fontenot.
+    </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="password" bsSize="large">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  type="password"
+                />
+              </Form.Group>
+              <Button
+                block
+                bsSize="large"
+                type="submit"
+              >
+                Login
+              </Button>
+
+              <a href="http://www.google.com">Click Here to Sign UP</a>
+            </Form>
+
+            <FailedLogin isLoggedIn={this.state.failed_login}/>
+          </div>
+          </>
+    );
+  }
+}
+
+export default Login;
