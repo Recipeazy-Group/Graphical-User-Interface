eafb17313471daa43b57507c0606cacf45ac1e4f
Richmond Aisabor <richmondaisabor@Richmonds-MacBook-Air.local>
2019-11-18 13:05:51 -0600

    init routes

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -3,16 +3,14 @@ import Signup from './components/SignupPage/Signup';
 import WorkoutGenerator from './components/WorkoutGenerator';
 import WorkoutPage from './components/WorkoutPage';
 import WorkoutPageEdit from './components/WorkoutPageEdit';
-import AccountHome from './components/accountPage/AccountHome';
+import AccountHome from './components/accountPage/AccountHome'; 
 import IngredientsPage from './components/accountPage/IngredientsPage';
 
 
 export const ROUTES = [
-  { path: '/login', component: Login},
-  { path: '/signup', component: Signup},
-  { path: '/home', component: AccountHome},
-  { path: '/workoutgen', component: WorkoutGenerator},
-  { path: '/recipepage/:recipeId', component: WorkoutPage},
+  { path: '/Dashboard', component: Dashboard},
+  { path: '/Splash', component: Splash},
+  { path: '/Tracker', component: Tracker},
   { path: '/', component: Login},
   { path: '/ingredients', component: IngredientsPage},
 ];
